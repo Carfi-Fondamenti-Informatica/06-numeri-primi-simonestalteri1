da2b0f8161999922eb17ff5addf9e49797b5024b
simonestalteri1 <117160924+simonestalteri1@users.noreply.github.com>
2022-12-03 09:00:03 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
-}
+int main() {
+    int num = 0;
+    cin >> num;
+        if (num==1) {
+            cout << "numero non primo" << endl;
+        } else if (numeri_primi(num, num/2)) {
+            cout << "numero primo" << endl;
+        } else {
+            cout << "numero non primo" << endl;
+        }
+        return 0;
+    }
